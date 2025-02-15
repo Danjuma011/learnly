a2fdb60867b97ed7df06ddd4b81f43f0486ce7df
Danjuma kuti <ayanfolarin@gmail.com>
2025-02-15 23:44:18 +0100

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: 'export',
-};
-
-export default nextConfig;
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export', // Add this line
+// const nextConfig: NextConfig = {
+//   output: 'export',
 // };
 
-// module.exports = nextConfig;
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Add this line
+};
+
+module.exports = nextConfig;
